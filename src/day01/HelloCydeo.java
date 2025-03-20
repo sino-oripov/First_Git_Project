33ef40bd057f59a90cce4ba53b73216352a47f1e
sino10 <sino-oripov@mail.ru>
2025-03-20 13:15:58 -0400

    Hello Cydeo updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,7 +3,7 @@ package day01;
 public class HelloCydeo {
    public static void main(String[] args){
   
-  system.out.println("Hello Cydeo")
+  System.out.println("Hello Cydeo");
 
   // Some changes are made. 
 
